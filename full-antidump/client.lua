88eadfb928acfc95fd07b4abecb1a28d8dfb5c67
omar-othmann <34454535+omar-othmann@users.noreply.github.com>
2022-06-18 16:06:07 +0200

    Delete full-antidump directory

--- full-antidump/client.lua
+++
@@ -1,2 +0,0 @@
-
--- Here you go, continue your code in client side what you want to do??
--- full-antidump/nui/main.js
+++
@@ -1,9 +0,0 @@
-
-// you don't need anymore 
-
-// $(document).ready(function(){
-    
-// })
-
-
-// just write your js code here.
