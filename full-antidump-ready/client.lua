8e928c173d548f28f2df64123f2b10069df1772c
omar-othmann <34454535+omar-othmann@users.noreply.github.com>
2022-06-18 16:17:08 +0200

    your script code

---
+++ full-antidump-ready/client.lua
@@ -0,0 +1,9 @@
+-- add your script code here, this can't be dumped by Cheater!!!!!
+-- warning: onStartResource, onStopResource event will no called in this file. you can add on public.lua to trigger after 2000 ms. for example
+
+-- AddEventHandler('onResourceStart', function(resource)
+--    if GetCurrentResouceName() == resource then
+---       Wait(2000)
+--        TriggerEvent('YourTriggerOnClient.lua') 
+--    end 
+-- end)
