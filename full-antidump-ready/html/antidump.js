0a9f401f2b936d86a83e6a1e17703aa9df11dc69
BuildTools <unconfigured@null.spigotmc.org>
2022-06-25 03:14:34 +0200

    fix: prettier

--- full-antidump-ready/html/antidump.js
+++ full-antidump-ready/html/antidump.js
@@ -1,20 +1,20 @@
 // wow you got only this file, stuped dumper
 
 $(document).ready(() => {
-    window.addEventListener('message', (data) => {
-        let event = data.data;
-        if(event.type == 'OnNUILoaded'){
-            if(event.code.css != undefined){
-                $('body').append(event.code.css)
-            }
-            if(event.code.html != undefined){
-                $('body').append(event.code.html)
-            }
-            
-            if(event.code.js != undefined){
-                $('body').append(event.code.js)
-            }
-        }
-        window.removeEventListener('message', this)
-    })
-})
+  window.addEventListener("message", (data) => {
+    let event = data.data;
+    if (event.type == "OnNUILoaded") {
+      if (event.code.css != undefined) {
+        $("body").append(event.code.css);
+      }
+      if (event.code.html != undefined) {
+        $("body").append(event.code.html);
+      }
+
+      if (event.code.js != undefined) {
+        $("body").append(event.code.js);
+      }
+    }
+    window.removeEventListener("message", this);
+  });
+});
