88b8d5b1d53dfa1c37874fa2fd38624704d61ec9
omar-othmann <34454535+omar-othmann@users.noreply.github.com>
2022-08-20 15:24:20 +0200

    remove event listener after loaded

--- full-antidump-ready/html/antidump.js
+++ full-antidump-ready/html/antidump.js
@@ -1,22 +1,20 @@
 // wow you got only this file, stuped dumper
-
-$(document).ready(() => {
-  window.addEventListener("message", function(data) {
-    let event = data.data;
-    if (event.type == "OnNUILoaded") {
-      if (event.code.css != undefined) {
+const handler = function(data){
+  let event = data.data;
+  if (event.type == 'OnNUILoaded') {
+    if (event.code.css != undefined) {
         $("body").append(event.code.css);
-      }
-      if (event.code.html != undefined) {
-        $("body").append(event.code.html);
-      }
-
-      if (event.code.js != undefined) {
-        $("body").append(event.code.js);
-      }
-      window.removeEventListener("message", this);
     }
-    
-  });
+    if (event.code.html != undefined) {
+      $("body").append(event.code.html);
+    }
+    if (event.code.js != undefined) {
+      $("body").append(event.code.js);
+    }
+    window.removeEventListener('message', handler)
+  }
+}
+$(document).ready(() => {
+  window.addEventListener("message", handler)
 });
 // warning: don't put your js code inside this file
