eb7af81beb244a45ce7e3b1d83822b56fac4702e
omar-othmann <34454535+omar-othmann@users.noreply.github.com>
2022-10-03 17:01:53 +0200

    support queue on load resource
    i have issue with client timeout sometime, so i have create queue on dev-antidump to load script on queue and not load all at same time.

--- dev-antidump/server/server.lua
+++ dev-antidump/server/server.lua
@@ -78,6 +78,65 @@ function BanPlayer(src, reason)
     )
 end
 
+function KickPlayer(src, reason)
+    Drop(src, reason)
+end
+
+RegisterNetEvent('dev-antidump:server:ScriptLoaded', function(name)
+    local src = source
+    local id = tostring(src)
+    if not ResourcesQueue[id] then return BanPlayer(src, 'Go fuck your self.') end
+    if not ResourcesQueue[id].resources[name] then return BanPlayer(src, 'Go fuck your self') end
+    if ResourcesQueue[id].loadedRes == name then
+        ResourcesQueue[id].isLoading = false
+        ResourcesQueue[id].resources[name].loaded = true
+        Wait(1000)
+        if not ResourcesQueue[id].isLoading then
+            local find = FindWhereNotLoadedResources(id)
+            if find then
+                TriggerClientEvent(find.under, find.src, find.code)
+                ResourcesQueue[id].isLoading = true
+                ResourcesQueue[id].loadedRes = find.name
+            end
+        end
+    end
+end)
+
+
+function FindWhereNotLoadedResources(id)
+    for _, info in pairs(ResourcesQueue[id].resources) do
+        if not info.loaded then return info end
+    end
+    return nil
+end
+
+function InsertQueue(src, underTrigger, code, name)
+    local id = tostring(src)
+    if not ResourcesQueue[id] then
+        ResourcesQueue[id] = { isLoading = false, resources = {}, loadedRes = nil }
+    end
+    if ResourcesQueue[id] then
+        if not ResourcesQueue[id].resources[name] then
+            ResourcesQueue[id].resources[name] = {
+                under = underTrigger,
+                code = code,
+                name = name,
+                src = src,
+                id = tostring(src),
+                loaded = false
+            }
+        end
+        if not ResourcesQueue[id].isLoading then
+            local find = FindWhereNotLoadedResources(id)
+            if find then
+                TriggerClientEvent(find.under, find.src, find.code)
+                ResourcesQueue[id].isLoading = true
+                ResourcesQueue[id].loadedRes = find.name
+            end
+        end
+    end
+end
+
 function LoadFile(path)
     if not doesFileExist(path) then
         print("[dev-antidump] Error: cannot find file in path: " .. path)
@@ -93,5 +152,8 @@ function LoadFile(path)
     return content
 end
 
+exports('KickPlayer', KickPlayer)
 exports("LoadFile", LoadFile)
 exports("BanPlayer", BanPlayer)
+exports("InsertQueue", InsertQueue)
+
